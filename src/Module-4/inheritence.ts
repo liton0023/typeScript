0cfcd24d5f7e4d60c11db11f8fd06183b99f4f44
Md Liton Mahmud <litonmahmud0023@gmail.com>
2024-03-28 18:05:16 +0600

    polymorphisom basice

--- src/Module-4/inheritence.ts
+++ src/Module-4/inheritence.ts
@@ -1,54 +1,54 @@
-class Parent {
-  name: string;
-  age: number;
-  address: string;
+// class Parent {
+//   name: string;
+//   age: number;
+//   address: string;
 
-  constructor(name: string, age: number, address: string) {
-    this.name = name;
-    this.age = age;
-    this.address = address;
-  }
-  makeSleep(hours: number): string {
-    return `This ${this.name} will sleep for ${hours}`;
-  }
-}
+//   constructor(name: string, age: number, address: string) {
+//     this.name = name;
+//     this.age = age;
+//     this.address = address;
+//   }
+//   makeSleep(hours: number): string {
+//     return `This ${this.name} will sleep for ${hours}`;
+//   }
+// }
 
-class Student extends Parent {
-  //   name: string;
-  //   age: number;
-  //   address: string;
+// class Student extends Parent {
+//   //   name: string;
+//   //   age: number;
+//   //   address: string;
 
-  constructor(name: string, age: number, address: string) {
-    super(name, age, address);
-    // this.name = name;
-    // this.age = age;
-    // this.address = address;
-  }
-  //   makeSleep(hours: number): string {
-  //     return `This ${this.name} will sleep for ${hours}`;
-  //   }
-}
+//   constructor(name: string, age: number, address: string) {
+//     super(name, age, address);
+//     // this.name = name;
+//     // this.age = age;
+//     // this.address = address;
+//   }
+//   //   makeSleep(hours: number): string {
+//   //     return `This ${this.name} will sleep for ${hours}`;
+//   //   }
+// }
 
-const student1 = new Student("Mr.X", 15, "Uganda");
-// student1.
-class Teacher extends Parent {
-  //   name: string;
-  //   age: number;
-  //   address: string;
-  postion: string;
+// const student1 = new Student("Mr.X", 15, "Uganda");
+// // student1.
+// class Teacher extends Parent {
+//   //   name: string;
+//   //   age: number;
+//   //   address: string;
+//   postion: string;
 
-  constructor(name: string, age: number, address: string, postion: string) {
-    super(name, age, address);
-    // this.name = name;
-    // this.age = age;
-    // this.address = address;
-    this.postion = postion;
-  }
-  //   makeSleep(hours: number): string {
-  //     return `This ${this.name} will sleep for ${hours}`;
-  //   }
+//   constructor(name: string, age: number, address: string, postion: string) {
+//     super(name, age, address);
+//     // this.name = name;
+//     // this.age = age;
+//     // this.address = address;
+//     this.postion = postion;
+//   }
+//   //   makeSleep(hours: number): string {
+//   //     return `This ${this.name} will sleep for ${hours}`;
+//   //   }
 
-  takeClass(numOfClass: number): string {
-    return `This ${this.name} will take ${numOfClass} class`;
-  }
-}
+//   takeClass(numOfClass: number): string {
+//     return `This ${this.name} will take ${numOfClass} class`;
+//   }
+// }
